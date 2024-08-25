9ba2b2eb0fca229c24a141be51ab7cf6a4ba1b9c
WiktorGrzankowski <w.grzankowski@gmail.com>
2024-08-25 15:47:29 +0000

    works, 250 RPS, but aerospike in-memory mode so maybe slower

--- src/main/java/allezon/UserActionsService.java
+++ src/main/java/allezon/UserActionsService.java
@@ -4,11 +4,13 @@ import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Collectors;
 
@@ -22,11 +24,23 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.aerospike.client.AerospikeClient;
+import com.aerospike.client.Bin;
 import com.aerospike.client.Host;
+import com.aerospike.client.Info;
+import com.aerospike.client.Key;
+import com.aerospike.client.Record;
 import com.aerospike.client.policy.ClientPolicy;
 import com.aerospike.client.policy.CommitLevel;
 import com.aerospike.client.policy.RecordExistsAction;
 import com.aerospike.client.policy.Replica;
+import com.aerospike.client.query.RecordSet;
+import com.aerospike.client.query.Statement;
+import com.aerospike.client.task.IndexTask;
+import com.aerospike.client.query.Filter;
+import com.aerospike.client.query.IndexType;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 import allezon.domain.Action;
 import allezon.domain.TimeBound;
@@ -36,6 +50,7 @@ import allezon.domain.UserTagEvent;
 @Service
 public class UserActionsService {
     private static final String NAMESPACE = "parsr";
+    private static final String SET = "user_tags";
 
     private static final Logger log = LoggerFactory.getLogger(UserActionsResource.class);
 
@@ -45,6 +60,15 @@ public class UserActionsService {
     public UserActionsService(@Value("${aerospike.seeds}") String[] aerospikeSeeds, @Value("${aerospike.port}") int port) {
         this.client = new AerospikeClient(defaultClientPolicy(), Arrays.stream(aerospikeSeeds).map(seed -> new Host(seed, port)).toArray(Host[]::new));
 
+        String indexesInfo = Info.request(client.getNodes()[0], "sindex/" + NAMESPACE);
+        if (!indexesInfo.contains("cookie_index")) {
+            IndexTask cookieIndexTask = client.createIndex(null, NAMESPACE, SET, "cookie_index", "cookie", IndexType.STRING);
+            cookieIndexTask.waitTillComplete();   
+        }
+        if (!indexesInfo.contains("action_index")) {
+            IndexTask actionIndexTask = client.createIndex(null, NAMESPACE, SET, "action_index", "action", IndexType.STRING);
+            actionIndexTask.waitTillComplete();   
+        }
     }
 
     private static ClientPolicy defaultClientPolicy() {
@@ -60,30 +84,74 @@ public class UserActionsService {
         return defaultClientPolicy;
     }
 
+
+
     public ResponseEntity<Void> addUserTag(UserTagEvent userTag) {
-        if (!events.containsKey(userTag.getCookie())) {
-            events.put(userTag.getCookie(), new ConcurrentHashMap<>());
-            events.get(userTag.getCookie()).put(Action.BUY, new LinkedList<>());
-            events.get(userTag.getCookie()).put(Action.VIEW, new LinkedList<>());
-        }
-        LinkedList<UserTagEvent> actionedEvent = events.get(userTag.getCookie()).get(userTag.getAction());
-        synchronized (actionedEvent) {
-            actionedEvent.add(userTag);
-            if (actionedEvent.size() > 400) {
-                actionedEvent = (LinkedList<UserTagEvent>) actionedEvent.subList(0, 200);
-            }
-            events.get(userTag.getCookie()).put(userTag.getAction(), actionedEvent);
+        try {
+            // log.info("try to add things");
+            ObjectMapper mapper = new ObjectMapper();
+            mapper.registerModule(new JavaTimeModule());
+            String serializedEvent = mapper.writeValueAsString(userTag);
+            
+            String keyString = userTag.getCookie() + ":" + userTag.getAction() + ":" + userTag.getTime();
+            Key key = new Key(NAMESPACE, SET, keyString);
+            
+            Bin eventBin = new Bin("event", serializedEvent);
+            Bin cookieBin = new Bin("cookie", userTag.getCookie());
+            
+            client.put(null, key, eventBin, cookieBin);
+            
+            // log.info("UserTagEvent added successfully with key: {}", keyString);
+        } catch (Exception e) {
+            e.printStackTrace();
+            log.info("Failed to add UserTagEvent to Aerospike.");
         }
         return ResponseEntity.noContent().build();
+    }
+
+    public List<UserTagEvent> getUserTags(String cookie, Action action, TimeBound timeBound) {
+        List<UserTagEvent> userTags = new ArrayList<>();
+    
+        try {
+            // log.info("try to get some things");
+            Statement statement = new Statement();
+            statement.setNamespace(NAMESPACE);
+            statement.setSetName(SET);
+            
+            // Applying filters based on cookie
+            statement.setFilter(Filter.equal("cookie", cookie));
+    
+            RecordSet rs = client.query(null, statement);
+    
+            ObjectMapper mapper = new ObjectMapper();
+            mapper.registerModule(new JavaTimeModule());
+
+            while (rs.next() && userTags.size() < 200) {
+                Record record = rs.getRecord();
+                String serializedEvent = record.getString("event");
+                UserTagEvent userTag = mapper.readValue(serializedEvent, UserTagEvent.class);
+                // log.info("for cookie: {}, event is: {}", cookie, userTag.toString());
+                if (userTag.getAction().equals(action) && 
+                    !userTag.getTime().isBefore(timeBound.getStartDate()) &&
+                    userTag.getTime().isBefore(timeBound.getEndDate())) {
+                    userTags.add(userTag);                    
+                }
+            }
+    
+            rs.close();
+            // log.info("Retrieved {} user tagsfor cookie {} and action {}", userTags.size(), cookie, action);
+        } catch (Exception e) {
+            e.printStackTrace();
+            log.info("Failed to retrieve UserTagEvents.");
+        }
+        userTags.sort((e1, e2) -> e2.getTime().compareTo(e1.getTime()));
+        return userTags;
     }
 
     public ResponseEntity<UserProfileResult> getUserProfile(String cookie,
                                                             String timeRangeStr,
                                                             int limit,
                                                             UserProfileResult expectedResult) {
-        if (!events.containsKey(cookie)) {
-            return ResponseEntity.ok(new UserProfileResult(cookie, new LinkedList<>(), new LinkedList<>()));
-        }
         TimeBound timeBound;
         try {
             timeBound = getTimeBounds(timeRangeStr);
@@ -92,12 +160,15 @@ public class UserActionsService {
             return ResponseEntity.badRequest().build();
         }
 
-        List<UserTagEvent> views = getEventsByAction(cookie, limit, timeBound, Action.VIEW);
-        List<UserTagEvent> buys = getEventsByAction(cookie, limit, timeBound, Action.BUY);
-        UserProfileResult myResult = new UserProfileResult(cookie, views, buys);  
-
-        logDiscrepancies(expectedResult, myResult);
-        
+        List<UserTagEvent> b = getUserTags(cookie, Action.BUY, timeBound);
+        List<UserTagEvent> v = getUserTags(cookie, Action.VIEW, timeBound);
+        if (expectedResult.getBuys().size() != b.size()) {
+            log.info("Wrong number of buys. Expected {} but found {}", expectedResult.getBuys().size(), b.size());
+        }
+        if (expectedResult.getViews().size() != v.size()) {
+            log.info("Wrong number of views. Expected {} but found {}", expectedResult.getViews().size(), v.size());
+        }
+        UserProfileResult myResult = new UserProfileResult(cookie, v, b);
         return ResponseEntity.ok(myResult);
     }
 

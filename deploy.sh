b285e6572d4f39bf1dc9a6f98267e0308565a1f2
Grzegorz Nowakowski <g.nowakowski@student.uw.edu.pl>
2024-09-03 16:48:57 +0000

    Merge hosts file and fix permissions and other bugs in deployment.yml

--- deploy.sh
+++ deploy.sh
@@ -15,10 +15,10 @@ echo "Building load balancer Docker image..."
 (cd load_balancer && sudo docker build -t my-proxy .)
 
 echo "Building app Docker image..."
-sudo docker build -t allezon .
+mvn -Pdocker package
 
 # Start aerospike
-ansible-playbook --extra-vars "$EXTRA_VARS" -i aerospike/hosts aerospike/aerospike.yaml
+ansible-playbook --extra-vars "$EXTRA_VARS" -i hosts aerospike/aerospike.yaml
 
 # Start the app
-ansible-playbook --extra-vars "$EXTRA_VARS" -i deployment/hosts deployment/deployment.yml
+ansible-playbook --extra-vars "$EXTRA_VARS" -i hosts deployment/deployment.yml

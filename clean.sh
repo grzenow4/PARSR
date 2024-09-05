dd9aeaaf8b5177919d9e1d4023333bef1f25dcee
Grzegorz Nowakowski <g.nowakowski@student.uw.edu.pl>
2024-09-05 17:51:35 +0000

    Add clean script

---
+++ clean.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <user> <password>"
+    exit 1
+fi
+
+USER=$1
+PASSWORD=$2
+
+EXTRA_VARS="ansible_user=$USER ansible_password=$PASSWORD ansible_ssh_extra_args='-o StrictHostKeyChecking=no'"
+
+ansible-playbook --extra-vars "$EXTRA_VARS" -i hosts deployment/clean.yml

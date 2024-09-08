735cc6b5cc78cc5d81dbc86aa997aac33b4e9066
Grzegorz Nowakowski <g.nowakowski@student.uw.edu.pl>
2024-09-08 11:22:18 +0000

    Change .yml to .yaml

--- deploy.sh
+++ deploy.sh
@@ -24,4 +24,4 @@ mvn -Pdocker package
 ansible-playbook --extra-vars "$EXTRA_VARS" -i hosts aerospike/aerospike.yaml
 
 # Start the app
-ansible-playbook --extra-vars "$EXTRA_VARS" -i hosts deployment/deployment.yml
+ansible-playbook --extra-vars "$EXTRA_VARS" -i hosts deployment/deployment.yaml

6cdfa6a7be6d0af9db4de20735787a31aebf1301
Grzegorz Nowakowski <g.nowakowski@student.uw.edu.pl>
2024-09-08 11:58:03 +0000

    Add kafka deployment

--- deploy.sh
+++ deploy.sh
@@ -23,5 +23,8 @@ mvn -Pdocker package
 # Start aerospike
 ansible-playbook --extra-vars "$EXTRA_VARS" -i hosts aerospike/aerospike.yaml
 
+# Start kafka
+ansible-playbook --extra-vars "$EXTRA_VARS" -i hosts kafka/kafka.yaml
+
 # Start the app
 ansible-playbook --extra-vars "$EXTRA_VARS" -i hosts deployment/deployment.yaml
